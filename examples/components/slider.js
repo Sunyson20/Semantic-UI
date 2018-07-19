43275d79f9731ecb2dfbe1f9564622b1fa919676
Sunyson Zeng <sunyson.zeng@gmail.com>
2018-07-20 00:22:38 +0800

    todo selected li

--- examples/components/slider.js
+++ examples/components/slider.js
@@ -28,32 +28,18 @@ $(document).ready(function () {
         cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
         easing: 'linear', //'for jquery animation',////
 
-        speed: 400, //ms'
+        speed: 200, //ms'
         auto: false,
         loop: true,
         slideEndAnimation: true,
-        pause: 2000,
+        pause: 500,
 
-        keyPress: false,
+        keyPress: true,
         controls: true,
         prevHtml: '',
         nextHtml: '',
 
-        rtl: false,
-        adaptiveHeight: false,
-
-        vertical: false,
-        verticalHeight: 500,
-        vThumbWidth: 100,
-
-        thumbItem: 10,
-        pager: true,
-        gallery: false,
-        galleryMargin: 5,
-        thumbMargin: 5,
-        currentPagerPosition: 'middle',
-
-        enableTouch: true,
+        enableTouch: false,
         enableDrag: true,
         freeMove: true,
         swipeThreshold: 40,
@@ -73,20 +59,18 @@ $(document).ready(function () {
             }
           }
         ],
-
-        onBeforeStart: function (el) {},
-        onSliderLoad: function (el) {},
-        onBeforeSlide: function (el) {},
-        onAfterSlide: function (el) {},
-        onBeforeNextSlide: function (el) {},
-        onBeforePrevSlide: function (el) {}
+        
       });
     });
 
-  $("#style-slider").on('click', 'li', function () {
+
+  $("#style-slider").on('click', 'li', function (e) {
+    console.log(e);
     console.log($(this).text());
     $(this).parent().find('li').removeClass('selected');
     $(this).addClass('selected');
+    // TODO: find the li text value equal this.text(), and set selected
+    return false;
   });
 
   $('.card .dimmer')
